de2f586ad89ef7476e32645f3ce32cd74af3c650
LancyShanu <lancyshanu@gmail.com>
2024-02-28 12:05:12 +0530

    my first project

---
+++ Market/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db=SQLAlchemy(app)
+
+from Market import routes
